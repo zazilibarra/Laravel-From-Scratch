69315c07c3597c283dbd930ca036a659e1b582c0
Zazil Ibarra <zaziil.97@gmail.com>
2020-05-16 01:42:45 -0500

    Layout Pages

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts/{post}', 'PostsController@show');
+//Route::get('/posts/{post}', 'PostsController@show');
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
