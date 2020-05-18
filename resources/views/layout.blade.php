0f61e2939ac2de765345ff5307c97f4b1a9fecf1
Zazil Ibarra <zaziil.97@gmail.com>
2020-05-18 01:24:34 -0500

    Asset Compilation with Laravel Mix and webpack

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,7 @@ Released   : 20140225
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="css/default.css" rel="stylesheet" />
 <link href="css/fonts.css" rel="stylesheet" />
+<link rel="stylesheet" href="/css/app.css">
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
@@ -42,5 +43,11 @@ Released   : 20140225
 	@yield('header')
 </div>
     @yield('content');
+
+	<div id="copyright" class="container">
+		<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+	</div>
+
+	<script src="/js/app.js"></script>
 </body>
 </html>
