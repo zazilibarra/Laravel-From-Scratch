a46cd900536cf632940a81a172c3c9a5bd43510b
Zazil Ibarra <zaziil.97@gmail.com>
2020-05-18 02:21:21 -0500

    Homework Solutions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,6 +7,13 @@ use App\Article;
 
 class ArticlesController extends Controller
 {
+    public function index()
+    {
+        $articles = Article::latest()->get();
+
+        return view('articles.index', ['articles' => $articles]);
+    }
+
     public function show($id)
     {
         $article = Article::find($id);
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,5 @@ Route::get('/about', function(){
     ]);
 });
 
+Route::get('/articles', 'ArticlesController@index');
 Route::get('/articles/{article}', 'ArticlesController@show');
