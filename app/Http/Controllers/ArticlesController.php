d2561bbd1e7dca496685103ed01ee6417a999cc6
Zazil Ibarra <zaziil.97@gmail.com>
2020-05-18 23:58:57 -0500

    Form Validation Essentials

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -32,6 +32,12 @@ class ArticlesController extends Controller
     //Persist the new resource
     public function store()
     {
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'
+        ]);
+
         $article = new Article();
 
         $article->title = request('title');
@@ -54,6 +60,12 @@ class ArticlesController extends Controller
     //Persist the edited resource
     public function update($id)
     {
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'
+        ]);
+        
         $article = Article::find($id);
 
         $article->title = request('title');
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -10,24 +10,40 @@
         <h1 class="heading has-text-weight-bold is-size-4">New Article</h1>
         <form action="/articles" method="POST">
             @csrf
+
             <div class="field">
                 <label for="title" class="label">Title</label>
                 <div class="control">
-                    <input type="text" name="title" id="title" class="input">
+                    <input type="text" name="title" id="title" class="input @error('title') is-danger @enderror" value="{{ old('title')}}">
+                    
+                    @error('title')
+                        <p class="help is-danger">{{$errors->first('title')}}</p>
+                    @enderror
                 </div>
             </div>
+
             <div class="field">
                 <label for="excerpt" class="label">Excerpt</label>
                 <div class="control">
-                    <textarea type="textarea" name="excerpt" id="excerpt"></textarea>
+                    <textarea class="textarea @error('excerpt') is-danger @enderror" name="excerpt" id="excerpt">{{ old('excerpt')}}</textarea>
+
+                    @error('excerpt')
+                        <p class="help is-danger">{{$errors->first('excerpt')}}</p>
+                    @enderror
                 </div>
             </div>
+
             <div class="field">
                 <label for="body" class="label">Body</label>
                 <div class="control">
-                    <textarea type="textarea" name="body" id="body"></textarea>
+                    <textarea class="textarea @error('body') is-danger @enderror" name="body" id="body">{{ old('body')}}</textarea>
+
+                    @error('body')
+                        <p class="help is-danger">{{$errors->first('body')}}</p>
+                    @enderror
                 </div>
             </div>
+
             <div class="field is-grouped">
                 <div class="control">
                     <button type="submit" class="button is-link">Submit</button>
